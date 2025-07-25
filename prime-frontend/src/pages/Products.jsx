faf00ec7839737b8ebb72a5d5b226e13d7caa727
Aadarsh Dangi <aadarsht0001@gmail.com>
2025-07-25 14:58:05 +0530

    font cgane

--- prime-frontend/src/pages/Products.jsx
+++ prime-frontend/src/pages/Products.jsx
@@ -8,12 +8,12 @@ const Products = () => {
   return (
     
     <div className="w-full h-full pt-20 bg-white overflow-hidden">
-        <h1 className="text-[20vh] mb-20 font-bold">PRODUCTS</h1>
+        <h1 className="text-4xl sm:text-6xl md:text-8xl lg:text-[15vh] xl:text-[20vh] mb-20 font-bold">PRODUCTS</h1>
 
        
 
 
-      <h3 className="text-[10vh] mt-20  font-bold">HYDRATION</h3>
+      <h3 className="text-2xl sm:text-4xl md:text-6xl lg:text-[7vh] xl:text-[10vh] mt-20 font-bold">HYDRATION</h3>
       <div style={{ height: "600px", position: "relative" }}>
         <CircularGallery
           bend={0}
@@ -24,7 +24,7 @@ const Products = () => {
       </div>
 
 
-        <h3 className="text-[10vh] mt-20 font-bold">RAPID REHYDRATION</h3>
+        <h3 className="text-2xl sm:text-4xl md:text-6xl lg:text-[7vh] xl:text-[10vh] mt-20 font-bold">RAPID REHYDRATION</h3>
        <div style={{ height: "600px", position: "relative" }}>
         <CircularGallery2
           bend={0}
@@ -35,7 +35,7 @@ const Products = () => {
       </div>
 
 
-  <h3 className="text-[10vh] mt-20 font-bold">ENERGY</h3>
+  <h3 className="text-2xl sm:text-4xl md:text-6xl lg:text-[7vh] xl:text-[10vh] mt-20 font-bold">ENERGY</h3>
        <div style={{ height: "600px", position: "relative" }}>
         <CircularGallery3
           bend={0}
